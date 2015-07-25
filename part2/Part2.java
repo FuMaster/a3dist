47fbea6cbb547a5dc5d4849cd543539b988840be
FuMaster <fuwengfai@gmail.com>
2015-07-25 03:55:51 +0000

    Done part2

---
+++ part2/Part2.java
@@ -0,0 +1,116 @@
+/*sampleIDsam
+  Code copied from https://github.com/facebookarchive/hadoop-20/blob/master/src/examples/org/apache/hadoop/examples/WordCount.java
+*/
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+public class Part2 {
+
+  public static class TokenizerMapper 
+       extends Mapper<Object, Text, Text, Text>{
+    
+    private Text isCancer = new Text();
+    private Text gID = new Text();  
+
+    public void map(Object key, Text value, Context context
+                    ) throws IOException, InterruptedException {
+      StringTokenizer itr = new StringTokenizer(value.toString(), ",");
+      
+      String sampleID = itr.nextToken();
+      //sID.set(sampleID);
+
+      double normal = 0.5;
+      int sum = 0;
+      int counter = 0;
+
+      while (itr.hasMoreTokens()) {
+	double num = Double.parseDouble(itr.nextToken());
+	isCancer.set("0");
+	if( num > normal ) {
+	   isCancer.set("1");
+	}
+	counter++;
+	gID.set("gene_"+counter);
+	context.write(gID, isCancer);
+      }
+
+    }
+  }
+  
+  public static class GeneCombiner 
+       extends Reducer<Text,Text,Text,Text> {
+    private Text result = new Text();
+                                                                  
+    public void reduce(Text key, Iterable<Text> values, 
+                       Context context
+                       ) throws IOException, InterruptedException {
+      int sum = 0;
+      int total = 0;
+
+      for (Text val : values) {
+        sum += Integer.parseInt(val.toString());
+	total++;
+      }
+      result.set(sum+"/"+total);
+      context.write(key, result);
+    }
+  }
+
+  public static class GeneReducer 
+       extends Reducer<Text,Text,Text,DoubleWritable> {
+    private DoubleWritable result = new DoubleWritable();
+
+    public void reduce(Text key, Iterable<Text> values, 
+                       Context context
+                       ) throws IOException, InterruptedException {
+
+      System.out.println( "Key: " + key.toString() );
+
+      double sum = 0.0;
+      int total = 0;
+      for (Text val : values) {
+	String parsed = val.toString();
+	System.out.println( "Sum/Total: " + parsed );
+	String[] list = parsed.split("/");
+	sum += Integer.parseInt(list[0]);
+	total += Integer.parseInt(list[1]);
+      }
+      
+      result.set( sum/total );
+      context.write(key, result);
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    conf.set("mapreduce.output.textoutputformat.separator", ",");
+    String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+    if (otherArgs.length != 2) {
+      System.err.println("Usage: gene score count <in> <out>");
+      System.exit(2);
+    }
+    Job job = new Job(conf, "gene score count");
+    job.setJarByClass(Part2.class);
+    job.setMapperClass(TokenizerMapper.class);
+    job.setCombinerClass(GeneCombiner.class);
+    job.setReducerClass(GeneReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(Text.class);
+    FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+    FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
+}
