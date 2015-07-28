f4f6fa2938962a5dc1b926b93d12b30e1ad3c0a6
FuMaster <fuwengfai@gmail.com>
2015-07-28 19:08:29 +0000

    pulling changes

--- part4/Part4_3.java
+++ part4/Part4_3.java
@@ -1,1 +1,34 @@
-//something
+import java.io.IOException;
+import org.apache.pig.EvalFunc;
+import org.apache.pig.data.Tuple;
+import org.apache.pig.data.DataBag;
+import org.apache.hadoop.io.WritableComparable;
+
+public class Part4_3 extends EvalFunc<String>{
+	public String exec(Tuple tuple) throws IOException {
+		try{
+			String str = tuple.toDelimitedString(",");
+			String str2 = str.replaceAll("\\(","");
+			String str3 = str2.replaceAll("\\)","");
+			String[] tokens = str3.split(",");
+
+			double max = 0;
+			String result = "";
+			int index = 1;
+			for(int i=0;i<tokens.length;i++)
+			{	
+				double geneVal = Double.parseDouble(tokens[i]);
+				if (geneVal > max)	{
+					result = "gene_"+index;
+					max = geneVal;
+				} else if(geneVal == max) {
+					result += ",gene_"+index;
+				}
+				index++;
+			}
+			return result;
+		} catch(Exception e) {
+			throw e;
+		}
+	}
+}
--- part4/Part4_3.pig
+++ part4/Part4_3.pig
@@ -1,1 +1,10 @@
 register Part4_3.jar;
+sampleDatas = LOAD '$input' USING PigStorage(',');
+-- AS (sampleID:chararray, geneValues:chararray); --reads in a line of input
+geneDatas = FOREACH sampleDatas GENERATE $0 AS sampleID, TOTUPLE($1..) AS geneValues:tuple();
+
+--rankedGenes = RANK geneDatas;
+geneTuples = FOREACH geneDatas GENERATE sampleID, Part4_3(geneValues);
+
+--DUMP highestValues;
+STORE highestValues INTO '$output' USING PigStorage(',');
