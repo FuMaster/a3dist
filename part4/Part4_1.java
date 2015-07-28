10015a341f94a422a847e6e08fc68827ee181df4
prli <lipeiran777@gmail.com>
2015-07-28 19:07:22 +0000

    fixed class names

--- part4/Part4_1.java
+++ part4/Part4_1.java
@@ -4,36 +4,36 @@ import org.apache.pig.data.Tuple;
 import org.apache.pig.data.DataBag;
 import org.apache.hadoop.io.WritableComparable;
 
-public class pigPart1 extends EvalFunc<String>{
-public String exec(Tuple tuple) throws IOException {
-try{
-String str = tuple.toDelimitedString(",");
-String str2 = str.replaceAll("\\(","");
-String str3 = str2.replaceAll("\\)","");
-String[] tokens = str3.split(",");
-
-double max = 0;
-String result = "";
-int index = 1;
-for(int i=0;i<tokens.length;i++)
-{	
-	double geneVal = Double.parseDouble(tokens[i]);
-	if(geneVal > max)
-	{
-		result = "gene_"+index;
-		max = geneVal;
-	}
-	else if(geneVal == max)
-	{
-		result += ",gene_"+index;
-	}
-	index++;
-}
-return result;
-}
-catch(Exception e)
-{
-	throw e;
-}
-}
+public class Part4_1 extends EvalFunc<String>{
+  public String exec(Tuple tuple) throws IOException {
+    try{
+      String str = tuple.toDelimitedString(",");
+      String str2 = str.replaceAll("\\(","");
+      String str3 = str2.replaceAll("\\)","");
+      String[] tokens = str3.split(",");
+      
+      double max = 0;
+      String result = "";
+      int index = 1;
+      for(int i=0;i<tokens.length;i++)
+      {	
+      	double geneVal = Double.parseDouble(tokens[i]);
+      	if(geneVal > max)
+      	{
+      		result = "gene_"+index;
+      		max = geneVal;
+      	}
+      	else if(geneVal == max)
+      	{
+      		result += ",gene_"+index;
+      	}
+      	index++;
+      }
+      return result;
+    }
+    catch(Exception e)
+    {
+    	throw e;
+    }
+  }
 }
